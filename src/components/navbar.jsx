8082270f297f25a7349904352b254a9d24312300
Michael <61466734+mcb2284@users.noreply.github.com>
2022-01-24 14:48:59 -0500

    Set up App.js as the parent component to the navbar and the counter

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,15 +1,14 @@
-import React, { Component } from "react";
-
-class NavBar extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-light bg-light">
-        <div className="container-fluid">
-          <span className="navbar-brand mb-0 h1">Navbar</span>
-        </div>
-      </nav>
-    );
-  }
-}
+const NavBar = ({ totalCounters }) => {
+  return (
+    <nav className="navbar navbar-light bg-light">
+      <div className="container-fluid">
+        <span className="navbar-brand mb-0 h1">
+          Navbar{" "}
+          <span className="badge bg-pill bg-secondary">{totalCounters}</span>
+        </span>
+      </div>
+    </nav>
+  );
+};
 
 export default NavBar;
